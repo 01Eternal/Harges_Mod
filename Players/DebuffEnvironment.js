04aff0e79460fea060a472dd16d605db52222d9f
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-01-21 19:16:12 -0300

    Update in DebuffEnvironment.

--- Players/DebuffEnvironment.js
+++ Players/DebuffEnvironment.js
@@ -1,44 +1,55 @@
-import { using } from '../TL/ModClasses.js'
+/** @format */
 
-using('TL')
-using('Terraria')
-using('Terraria.ID')
-export default class DebuffEnvironment extends ModPlayer{
-    PreUpdateEquips(player) {
-    
-        if (this.player.wet) {
-            
-            if (this.player.ZoneSnow){
-                if (this.player.chilled) {
-                        this.player.moveSpeed *= 0.90 // - 10 %
-                    }
-            }
-                if (this.player.ZoneJungle) {
-                    this.player.AddBuff(BuffID.Poisoned, 2, true, false)
-                }
-                
-                if (this.player.ZoneCrimson) {
-                    this.player.AddBuff(BuffID.Ichor, 60 * 5, true, false)
-                }
-                
-                if (this.player.ZoneCorrupt) {
-                    this.player.AddBuff(BuffID.CursedInferno, 2, true, false)
-            }   
-        }
-        
-        if (this.player.ZoneCrimson) {
-            this.player.AddBuff(BuffID.Bleeding, 2, true, false)
-        }
-        
-        if (this.player.ZoneCorrupt) {
-            this.player.AddBuff(BuffID.OnFire, 2, true, false)
-        }
-        
-        if (this.player.ZoneUnderworldHeight) {
-                this.player.AddBuff(BuffID.OnFire, 2, true, false)
-        }
- 
-        
+import { using } from '../TL/ModClasses.js';
 
-    }
+using('TL');
+using('Terraria');
+using('Terraria.ID');
+export default class DebuffEnvironment extends ModPlayer {
+	PreUpdateEquips(player) {
+	    
+		const ApplyDebuffDebuff = () => {
+			// In Water
+			if (this.player.wet) {
+				if (this.player.ZoneSnow) {
+					if (this.player.chilled) {
+						this.player.moveSpeed *= 0.9; // - 10 %
+					}
+				}
+
+				if (this.player.ZoneJungle) {
+					this.player.AddBuff(BuffID.Poisoned, 2, true, false);
+				}
+
+				if (this.player.ZoneCrimson) {
+					this.player.AddBuff(BuffID.Ichor, 60 * 5, true, false);
+				}
+
+				if (this.player.ZoneCorrupt) {
+					this.player.AddBuff(BuffID.CursedInferno, 2, true, false);
+				}
+
+				// Generic
+				if (this.player.ZoneCrimson) {
+					this.player.AddBuff(BuffID.Bleeding, 2, true, false);
+				}
+
+				if (this.player.ZoneCorrupt) {
+					this.player.AddBuff(BuffID.OnFire, 2, true, false);
+				}
+
+				if (this.player.ZoneUnderworldHeight) {
+					this.player.AddBuff(BuffID.OnFire, 2, true, false);
+				}
+
+				// if player have Slimed Debuff Reduce velocity in 25%
+				if (this.player.drippingSlime) {
+					// Main.NewText(`called`, 255, 255, 255);
+					this.player.moveSpeed -= 0.4; //60%
+				}
+			}
+		};
+		
+		ApplyDebuffDebuff();
+	}
 }
