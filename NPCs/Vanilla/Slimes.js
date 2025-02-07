ee36de778637906dbfec41d4b57984a974cfae4a
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-02-07 06:21:52 -0300

    Removed unessesary Imprints and variables and now using Harges Api!

--- NPCs/Vanilla/Slimes.js
+++ NPCs/Vanilla/Slimes.js
@@ -5,17 +5,7 @@ import { using } from '../../TL/ModClasses.js';
 using('Microsoft.Xna.Framework');
 using('Microsoft.Xna.Framework');
 using('TL');
-
-const NewProjectile =
-	Projectile[
-		'int NewProjectile(IEntitySource spawnSource, Vector2 position, Vector2 velocity, int Type, int Damage, float KnockBack, int Owner, float ai0, float ai1, float ai2)'
-	];
-
-const vector2 = (x, y) => Vector2.new()['void .ctor(float x, float y)'](x, y);
-const Multiply = Vector2['Vector2 Multiply(Vector2 value1, float scaleFactor)'];
-const Normalize = Vector2['Vector2 Normalize(Vector2 value)'];
-const Subtract = Vector2['Vector2 Subtract(Vector2 value1, Vector2 value2)'];
-
+using('Harges')
 const Slime = [            
     NPCID.BlueSlime,
             NPCID.BlackSlime,
