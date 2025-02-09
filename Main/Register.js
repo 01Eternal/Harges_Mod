16a724a023874a2706640f3207ded9faa3585bd9
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-02-08 21:11:34 -0300

    update Main/Register.js

--- Main/Register.js
+++ Main/Register.js
@@ -13,7 +13,13 @@ import { GlobalProjectile } from '../TL/GlobalProjectile.js';
 
 import DebuffZone from '../Players/DebuffEnvironment.js'
 import KingSlime from '../NPCs/Vanilla/Bosses/SlimeBoss.js';
+
+
+// dont crash please GOOOOOD
 import Slimy_Spike from '../Projectiles/Eternal/SlimySpike.js';
+import Falling_Slime from '../Projectiles/Eternal/FallingSlime.js';
+import Blood_Knife from '../Projectiles/Eternal/BloodKnife.js';
+import Blood_Shuriken from '../Projectiles/Eternal/BloodSpike.js';
 
 import Slimes_Rework from '../NPCs/Vanilla/Slimes.js'
 import EyeOfChuthulho from '../NPCs/Vanilla/Bosses/Eoc.js';
@@ -44,6 +50,9 @@ function Items() {
 }
 
 function projectiles() {
+	ModProjectile.register(Falling_Slime)
 	ModProjectile.register(Slimy_Spike)
-	ModProjectile.register(Eye_Lighting);
+	
+	ModProjectile.register(Blood_Shuriken);
+	ModProjectile.register(Blood_Knife)
 }
