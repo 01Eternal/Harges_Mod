0bb97f410892ebc501810f496434ac9f91b60898
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-02-10 06:50:57 -0300

    Added support to extra ai

---
+++ NPCs/Vanilla/Bosses/EOC/Marceless/AIM.js
@@ -0,0 +1,10 @@
+import { using } from '../../../../../TL/ModClasses.js';
+
+using('Microsoft.Xna.Framework');
+using('TL');
+using('Terraria.ID');
+using('Harges');
+
+export default class EOCM extends GlobalNPC {
+
+}
