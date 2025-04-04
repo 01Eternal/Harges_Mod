3f2a164e556a32474724f7d7b1f304a9450e470c
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-04-03 22:48:16 -0300

    Updating register to 1.5.2

--- Main/Register.js
+++ Main/Register.js
@@ -10,12 +10,14 @@ import { GlobalItem } from '../TL/GlobalItem.js';
 import { GlobalNPC } from '../TL/GlobalNPC.js';
 import { GlobalProjectile } from '../TL/GlobalProjectile.js';
 
+import DebuffZone from '../Players/DebuffEnvironment.js';
+import Harges_Player from '../Players/HargesPlayer.js';
 
-import DebuffZone from '../Players/DebuffEnvironment.js'
-
-import Slimes_Rework from '../NPCs/Vanilla/Slimes.js'
+import Slimes_Rework from '../NPCs/Vanilla/Slimes.js';
 import EyeOfChuthulho from '../NPCs/Vanilla/Bosses/EOC/AI.js';
 import KingSlime from '../NPCs/Vanilla/Bosses/KingSlime/AI.js';
+
+import KingSlimeHeart from '../Items/Eternal/KingSlimeHeart.js';
 
 // =============================================================================================
 
@@ -23,21 +25,23 @@ export function Register() {
 	Items();
 	NPCs();
 	// projectiles();
-	ModPlayers()
+	ModPlayers();
 }
 
 function NPCs() {
-    GlobalNPC.register(Slimes_Rework)
+	GlobalNPC.register(Slimes_Rework);
 	GlobalNPC.register(EyeOfChuthulho);
 	GlobalNPC.register(KingSlime);
 }
 
 function ModPlayers() {
-    ModPlayer.register(DebuffZone)
+	ModPlayer.register(DebuffZone);
+	ModPlayer.register(Harges_Player);
 }
 
 function Items() {
-	// ModItem.register(itemTest);
+	// ModItem.register(itemTest)
+	ModItem.register(KingSlimeHeart);
 }
 
 function projectiles() {}
